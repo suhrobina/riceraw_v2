bb647ffb0e9fffcc297b3c098e730b23c735122c
Suhrob R. Nuraliev <LongOverdueVitalEnergy@Gmail.com>
2021-06-26 19:39:46 +0500

    Update pointer devices script

--- dotfiles/.local/bin/scripts/pointer-devices.sh
+++ dotfiles/.local/bin/scripts/pointer-devices.sh
@@ -2,13 +2,17 @@
 # =============================================================================
 #          FILE: pointer-devices.sh
 #
-#         USAGE: Use xinput to find pointer device, determine ID and set 
+#         USAGE: Use xinput to find pointer device, determine ID and set
 #                properties:
-#                    $ xinpit list 
+#                    $ xinpit list
 #                    $ xinput list-props
-#                
+#
 #                Script running:
 #                    $ pointer-devices.sh
+#
+#                Check man pages:
+#                    $ man libinput
+#                    $ man 4 libinput
 #
 #   DESCRIPTION: Pointer devices configuration script
 #
@@ -16,27 +20,57 @@
 #     COPYRIGHT: Copyright (c) 2019
 #       LICENSE: GNU General Public License
 #       CREATED: 29 Oct 2019
+#       REVISED: 25 Jun 2021
 # =============================================================================
 
 
-# -- Turn OFF TouchPad --------------------------------------------------------
+# -- Lenovo T440p Touchpad ----------------------------------------------------
+
 POINTER_NAME="Synaptics TM3053-003"
-POINTER_PROP_NAME="Synaptics Off"
-POINTER_PROP_VALUE="1"
-
 POINTER_ID="$(xinput list | grep "$POINTER_NAME" | sed 's/^.*id=//g;s/\[.*//g')"
-POINTER_PROP_ID=$(xinput list-props ${POINTER_ID} | \
-		grep "${POINTER_PROP_NAME}" | head -1 | sed "s/^.*(//;s/).*//")
-xinput set-prop ${POINTER_ID} ${POINTER_PROP_ID} ${POINTER_PROP_VALUE}
 
+# Turn OFF
+POINTER_PROP_NAME="Synaptics Off" # default is 0
+xinput set-prop ${POINTER_ID} "${POINTER_PROP_NAME}" 0
 
-# -- Change TrackPoint sensitivity --------------------------------------------
+# -- Lenovo T440p TrackPoint --------------------------------------------------
+
 POINTER_NAME="TPPS/2 IBM TrackPoint"
-POINTER_PROP_NAME="libinput Accel Speed"
-POINTER_PROP_VALUE="-0.5"
+POINTER_ID="$(xinput list | grep "$POINTER_NAME" | sed 's/^.*id=//g;s/\[.*//g')"
+
+# Switch to flat acceleration mode
+POINTER_PROP_NAME="libinput Accel Profile Enabled" # default is 1, 0
+xinput set-prop ${POINTER_ID} "${POINTER_PROP_NAME}" 0, 1
+
+# Set the pointer speed
+POINTER_PROP_NAME="libinput Accel Speed" # default is 0.000000
+xinput set-prop ${POINTER_ID} "${POINTER_PROP_NAME}" "0.5"
+
+# -- Logitech MX300 -----------------------------------------------------------
+
+POINTER_NAME="B16_b_02 USB-PS/2 Optical Mouse"
 
 POINTER_ID="$(xinput list | grep "$POINTER_NAME" | sed 's/^.*id=//g;s/\[.*//g')"
-POINTER_PROP_ID=$(xinput list-props ${POINTER_ID} | \
-		grep "${POINTER_PROP_NAME}" | head -1 | sed "s/^.*(//;s/).*//")
-xinput set-prop ${POINTER_ID} ${POINTER_PROP_ID} ${POINTER_PROP_VALUE}
+
+# Switch to flat acceleration mode
+POINTER_PROP_NAME="libinput Accel Profile Enabled" # default is 1, 0
+xinput set-prop ${POINTER_ID} "${POINTER_PROP_NAME}" 0, 1
+
+# Set the pointer speed
+POINTER_PROP_NAME="libinput Accel Speed" # default is 0.000000
+xinput set-prop ${POINTER_ID} "${POINTER_PROP_NAME}" "0.55"
+
+# -- Kensington Slimblade Trackball -------------------------------------------
+
+POINTER_NAME="Kensington Slimblade Trackball"
+
+POINTER_ID="$(xinput list | grep "$POINTER_NAME" | sed 's/^.*id=//g;s/\[.*//g')"
+
+# Switch to flat acceleration mode
+POINTER_PROP_NAME="libinput Accel Profile Enabled" # default is 1, 0
+xinput set-prop ${POINTER_ID} "${POINTER_PROP_NAME}" 0, 1
+
+# Set the pointer speed
+POINTER_PROP_NAME="libinput Accel Speed" # default is 0.000000
+xinput set-prop ${POINTER_ID} "${POINTER_PROP_NAME}" "0.5"
 
